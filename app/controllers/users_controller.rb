1786c45cf2e0ddf0f2e09de7f612fa6c0d284363
Matt Duncan <busman84@gmail.com>
2015-06-06 21:45:44 -0500

    Added a sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
-  end
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+  	def new
+  		@user = User.new
+  	end
+
+	def show
+	
+	end
+
+	def create
+    	@user = User.new(user_params)
+    	if @user.save
+     		redirect_to @user
+   		else
+      		render 'new'
+    	end
+  	end
+
 end
+
+private
+  	def set_user
+      @user = User.find(params[:id])
+    end
+
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:id, :username, :email, :password, :first_name, :last_name)
+    end
